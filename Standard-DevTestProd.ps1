ec43388a29c1997d6c2451d17080e17867872376
PolarbearDK <philip@dsdevelopment.dk>
2016-04-11 10:29:03 +0200

    Add standard script for Dev/Test/Prod

---
+++ Standard-DevTestProd.ps1
@@ -0,0 +1,8 @@
+﻿$ErrorActionPreference = "Stop"
+Set-StrictMode -Version latest
+
+$scriptPath = Split-Path $script:MyInvocation.MyCommand.Path
+& $scriptPath\Build-Configuration-Tool.ps1 Debug -Copy Dev -Rename Test
+if(!$?){throw "Script returned exit code $LASTEXITCODE"}
+& $scriptPath\Build-Configuration-Tool.ps1 Release -Rename Prod
+if(!$?){throw "Script returned exit code $LASTEXITCODE"}
